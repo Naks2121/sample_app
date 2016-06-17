f29b3ee6801a9ded3b5100fc32a22e8b6691131b
Roman Semeniuk <alonsit316@yandex.ua>
2016-06-17 14:20:00 +0300

    Make a basic USer model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,27 +2,35 @@ require 'spec_helper'
 
 describe "Static pages" do
 subject { page }
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
-    it { should have_content ('Sample App') }
-    it { should have _title (full_title(''))}
-    it { should_not have title('| Home')    }
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
   end
+
 
   describe "Help page" do
     before { visit help_path }
-    it { should have_content ('Help')      }
+    it { should have_selector('h1', text: 'Help') }
     it { should have_title (full_tile('Help')) }
   end
 
   describe "About page" do
-    it {should have_content ('About Us')   }
+    it { should have_selector('h1', text: 'About Us') }
     it {should have_title (full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
-    it { should have_content ('Contact')    }
+    it { should have_selector('h1', text: 'Contact') }
     it { should have_title (full_title('Contact')) }
   end
 end
